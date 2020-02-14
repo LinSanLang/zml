eb0bd1d69b637eab9ecfe0bbbd43f785f5896a91
zml <2927361944@qq.com>
2020-02-14 17:55:47 +0800

    模板文件与静态文件

--- end/demo1/bookdemo/booktest/urls.py
+++ end/demo1/bookdemo/booktest/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     # 第一个参数就是路由 第二个参数就是视图函数
     # 第一个参数章如果有正则分组 小括号 则正则分组匹配的内容就会作为实参传递给视图函数
     url(r'detail/(\d+)/',views.detail,name='detail'),
-    url(r'^$',views.index,name='index')
+    url(r'^$',views.index,name='index'),
+    url(r'^deletebook/(\d+)/$',views.deletebook,name='deletebook'),
+    url(r'^deletehero/(\d+)/$',views.deletehero,name='deletehero'),
+    url(r'^edithero/(\d+)/$',views.edithero,name='edithero'),
+    url(r'^addhero/(\d+)/$',views.addhero,name='addhero')
 ]
--- end/demo1/bookdemo/booktest/views.py
+++ end/demo1/bookdemo/booktest/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
 from django.template import loader
 from .models import Book,Hero
 # Create your views here.
@@ -6,7 +6,7 @@ from .models import Book,Hero
 # MVT  中的V视图模块
 # 在此处接受请求 处理数据 返回响应
 
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 
 def index(res):
     # # return HttpResponse("这里是首页2")
@@ -24,6 +24,53 @@ def index(res):
 
 def list(res):
     return HttpResponse("这里是详情页2")
+
+def deletebook(res,bookid):
+    book=Book.objects.get(id=bookid)
+    book.delete()
+    # return HttpResponse("删除成功")
+    # 删除之后仍然回到列表页
+    # return HttpResponseRedirect(redirect_to='/')
+    # return redirect(to='/')
+    url = reverse('booktest:index')
+    return redirect(to=url)
+
+def deletehero(res,heroid):
+    hero=Hero.objects.get(id=heroid)
+    # 先获取在删除
+    bookid = hero.book.id
+    hero.delete()
+    url = reverse('booktest:detail',args=(bookid,))
+    return redirect(to=url)
+
+def addhero(res,bookid):
+    # 视图函数中可以同时存在get和post请求 默认为get
+    if res.method == 'GET':
+        return render(res,'addhero.html')
+    elif res.method == 'POST':
+        hero = Hero()
+        hero.name = res.POST.get("heroname")
+        hero.content = res.POST.get("herocontent")
+        hero.gender = res.POST.get("sex")
+        hero.book = Book.objects.get(id=bookid)
+        hero.save()
+        url = reverse("booktest:detail",args=(bookid,))
+        return redirect(to=url)
+
+def edithero(res,heroid):
+    hero = Hero.objects.get(id=heroid)
+    # 使用get方法进入英雄的编辑页面
+    if res.method == "GET":
+        return render(res,'edithero.html',{"hero":hero})
+    elif res.method == "POST":
+        hero.name = res.POST.get("heroname")
+        hero.content = res.POST.get("herocontent")
+        hero.gender = res.POST.get("sex")
+        hero.save()
+        url = reverse("booktest:detail",args=(hero.book.id,))
+        return redirect(to=url)
+
+
 
 def detail(res,bookid):
     # return HttpResponse("这里是详情页"+bookid)
