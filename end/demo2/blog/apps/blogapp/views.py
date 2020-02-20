0b3723667f2c049c367f97c0cfbee75da524ff7f
zml <2927361944@qq.com>
2020-02-20 11:54:38 +0800

    博客配置

--- end/demo2/blog/apps/blogapp/views.py
+++ end/demo2/blog/apps/blogapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(resquest):
+    return render(resquest,'index.html')
+    # return HttpResponse("首页")
+
+def detail(resquest,id):
+    return render(resquest,'single.html')
+    # return HttpResponse("详情页")
+
+def contact(resquest):
+    return render(resquest,'contact.html')
+    # return HttpResponse("首页")
--- end/demo2/blog/blog/urls.py
+++ end/demo2/blog/blog/urls.py
@@ -21,5 +21,6 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ueditor/', include('DjangoUeditor.urls')),
+    path('',include('blogapp.urls',namespace='blogapp')),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
 ]
